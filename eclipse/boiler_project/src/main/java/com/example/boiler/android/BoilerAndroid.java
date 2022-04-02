6be7acc6b1e4015d08de8dde910e19e8a4defd38
Park jae wan <hanulcom7@naver.com>
2022-04-02 19:20:43 +0900

    자료 추가
    추가

--- eclipse/boiler_project/src/main/java/com/example/boiler/android/BoilerAndroid.java
+++ eclipse/boiler_project/src/main/java/com/example/boiler/android/BoilerAndroid.java
@@ -27,7 +27,6 @@ public class BoilerAndroid {
 	//http://10.10.141.76:8080/android/updateStatus/ON&OFF 이렇게 on , off 가능 전체 데이터 다 바꿀수 있게 잡음
 	@GetMapping("/android/updateStatus/{current_status}&{status}")
 	void getUpdateAge(@PathVariable("current_status")String current_status, @PathVariable("status")String status) { //db에 있는 데이터값 바꾸기
-		
 		mapper.updateMember(current_status, status);
 	}
 
